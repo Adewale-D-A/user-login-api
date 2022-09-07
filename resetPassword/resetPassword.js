f52a9bf79700bf5943d6e555cd993d3efbba632d
adewale-d-a <adewale-d-a.com>
2022-09-07 08:06:10 +0100

    change reset token once password is reset

--- resetPassword/resetPassword.js
+++ resetPassword/resetPassword.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 
+var randomstring = require("randomstring");
+
 const db = require("../dbCredentials");
 db.connect();
 
@@ -37,6 +39,7 @@ router.post("/resetPassword", (req, res) => {
           }
           if (result.length >= 1) {
             saltRounds = 10;
+            const generatedCode = randomstring.generate(10);
             bcrypt.hash(newPassword, saltRounds).then((hash) => {
               db.query(
                 `UPDATE user_registration SET password= "${hash}" WHERE verificationCode="${resetCode}";`,
@@ -48,10 +51,23 @@ router.post("/resetPassword", (req, res) => {
                     });
                   }
                   if (data) {
-                    res.status(200).send({
-                      success: true,
-                      message: "password changed successfully",
-                    });
+                    db.query(
+                      `UPDATE user_registration SET verificationCode="${generatedCode}" WHERE verificationCode="${resetCode}";`,
+                      (err, result) => {
+                        if (err) {
+                          res.status(401).send({
+                            success: false,
+                            message: "unable to update db",
+                          });
+                        }
+                        if (result) {
+                          res.status(200).send({
+                            success: true,
+                            message: "password changed successfully",
+                          });
+                        }
+                      }
+                    );
                   }
                 }
               );
